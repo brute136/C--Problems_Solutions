595767508acfab76c1485b4c989889f424fe887f
TeenX <81262408+brute136@users.noreply.github.com>
2021-08-31 23:55:43 +0530

    Add files via upload

---
+++ Basic-Data-Type.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int a;
+	long b;
+	char c;
+	float d;
+	double e;
+	scanf("%d %lu %c %f  %lf", &a, &b, &c, &d, &e);   
+	printf("%d\n", a);
+	printf("%lu\n", b);
+	printf("%c\n", c);
+	printf("%0.2f\n", d);
+	printf("%0.9lf", e);
+	return 0; 
+    //My 3rd Problem Solved In HackerRank At The Age Of 13 
+	//Date 31/August/2021
+	//TeenX
+}
---
+++ Name-Age-dateOfBirth-printing-Using-input.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	char Name[100];
+	int Age;
+	int born[10];
+	printf("Enter Your Name : ");
+	scanf("%s", &Name);
+	printf("Enter Your AGE : ");
+	scanf("%d", &Age);
+	printf("Enter Your Death Of Birth : ");
+	scanf("%s", &born);
+	printf("Your Name Is %s\n", Name);
+	printf("Your Age Is %d\n", Age);
+	printf("Your DOB IS %s", born);
+	//My 2rd Problem Solved In HackerRank At The Age Of 13 
+	//Date 31/August/2021
+	//TeenX
+	return 0; 
+}
